ad9733935014bd1e0ab0ffcc4e4904e7772ab451
khyan22 <khyanalvarez@gmail.com>
2022-06-09 04:24:45 -0400

    added Vote model and connected it to User and Post

--- models/index.js
+++ models/index.js
@@ -1,13 +1,49 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
-//creates associations
+//connects to post using one to many relationship
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
+//connects to post using many to many relationship and through Vote table
+User.belongsToMany(Post, {
+    through: Vote, 
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+//connects to User table 
 Post.belongsTo(User, {
     foreignKey: 'user_id'
-})
+});
 
-module.exports = { User, Post };
+//connects to user table
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+//connects to user
+Vote.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+//connects to post
+Vote.belongTo(Post, {
+    foreignKey: 'post_id'
+});
+
+//connects user to vote
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+//connects post to user
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };

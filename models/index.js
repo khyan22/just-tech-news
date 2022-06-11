4bfec69ae186fa23955d2d4fb508942f20809c57
khyan22 <khyanalvarez@gmail.com>
2022-06-11 03:18:18 -0400

    pushed from wrong directory

--- models/index.js
+++ models/index.js
@@ -1,14 +1,10 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 //connects to post using one to many relationship
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
-
-//connects to User table 
-Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
@@ -17,6 +13,29 @@ User.belongsToMany(Post, {
     through: Vote, 
     as: 'voted_posts',
     foreignKey: 'user_id'
+});
+
+//connects user to vote
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+//connects to User table 
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+//connects post to user
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
 });
 
 //connects to user table
@@ -36,14 +55,12 @@ Vote.belongsTo(Post, {
     foreignKey: 'post_id'
 });
 
-//connects user to vote
-User.hasMany(Vote, {
+Comment.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-//connects post to user
-Post.hasMany(Vote, {
+Comment.belongsTo(Post, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
